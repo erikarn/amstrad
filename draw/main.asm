0bdc48fe9b246ebbe0848b8144643d8e2b59f31d
Adrian Chadd <adrian@FreeBSD.org>
2023-12-31 08:37:02 -0800

    [draw] initial draw hacks
    just screen setup, untested

---
+++ draw/main.asm
@@ -0,0 +1,8 @@
+include "jumpblock.asm"
+
+org $8000
+
+entry:
+	ret
+
+include "screen.asm"
---
+++ draw/screen.asm
@@ -0,0 +1,64 @@
+
+; a test to have something to draw line based art
+; based on a vector list.
+
+; Populate the ink list
+; HL = pointer to border and ink list
+; B = number of inks in the pen list - so mode 1, mode 2 screens don't require
+; all of the inks populated.
+;
+; exit: HL points to address after ink list,
+; everything else is corrupted.
+;
+populate_ink_list:
+					; populate border ink
+	PUSH BC
+	LD A, (HL)
+	LD B, A
+	LD C, A
+	PUSH HL
+	CALL SCR_SET_BORDER
+	POP HL
+	POP BC
+
+	INC HL				; next ink
+
+					; populate screen inks
+	XOR A				; start at 0
+loop:
+	PUSH BC
+	PUSH AF
+	LD A, (HL)			; get the pen colour
+	LD B, A
+	LD C, A
+	POP AF				; A is again the ink colour
+	PUSH HL
+	CALL SCR_SET_INK
+	POP HL
+	POP BC
+	INC HL				; next ink in memory
+	INC A				; next pen number
+	DJNZ loop
+
+	RET
+
+; Populate the screen mode
+;
+; HL - points to screen address mode byte
+;
+; Return: HL points after screen address mode byte,
+; everything else corrupted
+;
+screen_mode_setup:
+	LD A, (HL)
+	PUSH HL
+	CALL SCR_SET_MODE
+	POP HL
+	INC HL
+	RET
+
+
+screen_setup:
+	call screen_mode_setup
+	call populate_ink_list
+	RET
